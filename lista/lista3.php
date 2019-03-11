bb0584363cef7ef60a743d5ac78ff64ebe38a6af
1831348 <1831348@LABCMAQ12.itp.local>
2019-03-11 10:50:11 -0300

    lista3

---
+++ lista/lista3.php
@@ -0,0 +1,90 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>LISTA3</title>
+    </head>
+    <body>
+        <?php
+        echo  " PARTE 1: <br> " ;
+        $numero=7;
+        function NumeroPrimo ($numero){
+        $count=0;
+        for($i=1;$i<=$numero;$i++){
+           if($numero % $i==0) {
+               $count=$count+1;
+           }
+        }
+           if($count==2){
+               echo " NÃºmero Ã© Primo ";
+           } else {
+               echo" NÃºmero Ã© composto Composto ";
+           }
+        }             
+       NumeroPrimo ($numero);         
+         
+       
+       
+       
+        echo  "<br> PARTE 2: <br> " ;
+        
+        
+
+    function primo($num) {
+                $cont=0;  
+                
+                for ($i=1; $i<=$num; $i++){
+                    if ($num%$i==0){
+                        $cont++;
+                   }
+                }
+                
+                if ($cont == 2){
+                    echo "<p> NÃºmero $num Ã© Primo</p>";
+                } else {
+                    echo "<p> NÃºmero $num NÃƒO Ã© Primo</p>";
+                }
+    }
+    
+    for ($i=1; $i<=100; $i++){
+      primo ($i);  
+    }
+
+       ?>
+        <ul>
+            <?php
+
+             echo  "<br> PARTE 3: <br> " ;
+
+             $color = array(
+                 'azul',
+                 'verde',
+                 'amarelo',
+                 'laranja',
+                 'rosa',
+                 'dourado',
+                 'prata',
+                 'cinza',
+                 'roxo',
+                 'marrom'
+                 );
+                 function Cores ($color){
+
+                     for($i=0;$i<count($color);$i++){
+            ?>
+            <li>
+            <?php
+                echo $color[$i];  
+            ?>           
+            </li>
+
+             <?php
+                     }
+
+                 }
+                 Cores($color);
+
+             ?>
+        </ul>
+    </body>
+</html>
+
